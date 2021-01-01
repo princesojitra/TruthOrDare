f9aa938036fb931e42f8206016b15a3b6b4f1687
Prince Sojitra <prince.sojitra@wipro.com>
2021-01-02 00:54:07 +0530

    Add Comment Truths

--- TruthOrDare/ViewModel.swift
+++ TruthOrDare/ViewModel.swift
@@ -12,12 +12,8 @@ class ViewModel {
     
     //Truth
     private let truths = [
-        "What are you most self-conscious about?",
-        "What would you do if you were the opposite sex for a month?",
-        "What is the most expensive thing you have stolen?",
-        "What is the most childish thing you still do?",
-        "Have you ever let someone take the blame for something you did?"
-    ]
+            "Have you ever let someone take the blame for something you did?"
+        ]
     
     //Dare
     private let dares = [
